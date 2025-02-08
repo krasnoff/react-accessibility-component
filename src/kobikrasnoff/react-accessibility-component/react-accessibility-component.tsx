5404501acf1014e8989f80bc67781a3f46c871ad
Kobi Krasnoff <krasnoff.kobi@gmail.com>
2025-02-08 19:16:38 +0200

    minor fix

--- src/kobikrasnoff/react-accessibility-component/react-accessibility-component.tsx
+++ src/kobikrasnoff/react-accessibility-component/react-accessibility-component.tsx
@@ -1,7 +1,13 @@
-const AccessibilityComponent = () => {
+import React from 'react';
+
+interface AccessibilityComponentProps {
+    text?: string;
+}
+
+const AccessibilityComponent: React.FC<AccessibilityComponentProps> = ({ text }) => {
     return (
-        <div role="region" aria-label="React Accessibility Component">
-            <p>component</p>
+        <div role="region" aria-label="Sample Accessibility Component">
+            <p>React accessibility component</p>
         </div>
     );
 };
