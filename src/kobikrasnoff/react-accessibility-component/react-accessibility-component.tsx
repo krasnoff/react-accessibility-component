5bfc8b03f4c233a7a482af5a0bba6da38b129ee3
Kobi Krasnoff <krasnoff.kobi@gmail.com>
2025-02-10 22:21:05 +0200

    implement shadow dom component

--- src/kobikrasnoff/react-accessibility-component/react-accessibility-component.tsx
+++ src/kobikrasnoff/react-accessibility-component/react-accessibility-component.tsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import { createRoot } from 'react-dom/client';
 
 interface AccessibilityComponentProps {
     text?: string;
 }
 
 const AccessibilityComponent: React.FC<AccessibilityComponentProps> = ({ text }) => {
+    const hostRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        if (hostRef.current && hostRef.current.shadowRoot === null) {
+            const shadowDom = hostRef.current?.attachShadow({ mode: 'open' });
+            if (shadowDom) {
+                const root = createRoot(shadowDom);
+                root.render(shadowDomJsxElement);
+            }
+        }
+    }, []);
+
+    const shadowDomJsxElement = <div>Hello World</div>;
+
     return (
-        <div role="region" aria-label="Sample Accessibility Component">
-            <p>React accessibility component</p>
-        </div>
+        <div role="region" aria-label="Sample Accessibility Component" ref={hostRef}></div>
     );
 };
 
