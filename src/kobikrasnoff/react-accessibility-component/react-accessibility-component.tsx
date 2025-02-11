c45b51db613a9b8679588733e80d9c8b5e093a19
Kobi Krasnoff <krasnoff.kobi@gmail.com>
2025-02-11 23:01:31 +0200

    insert first SVG icon

--- src/kobikrasnoff/react-accessibility-component/react-accessibility-component.tsx
+++ src/kobikrasnoff/react-accessibility-component/react-accessibility-component.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useRef } from 'react';
 import { createRoot } from 'react-dom/client';
+import styles from './react-accessibility-component.module.scss';
+import accessibilityImage from './assets/accessibility_new_24dp_5F6368_FILL0_wght400_GRAD0_opsz24.svg';
 
 interface AccessibilityComponentProps {
     text?: string;
@@ -18,10 +20,28 @@ const AccessibilityComponent: React.FC<AccessibilityComponentProps> = ({ text })
         }
     }, []);
 
-    const shadowDomJsxElement = <div>Hello World</div>;
+    const shadowDomJsxElement = 
+    <div>
+        <style>
+            {`
+                .container {
+                    border: 1px solid #ccc;
+                    cursor: pointer;
+                    display: flex;
+                }
+                .container img {
+                    width: 24px;
+                    height: 24px;
+                }
+            `}
+        </style>
+        <div className="container">
+            <img src={accessibilityImage} alt="Accessibility icon" />
+        </div>
+    </div>;
 
     return (
-        <div role="region" aria-label="Sample Accessibility Component" ref={hostRef}></div>
+        <div role="region" aria-label="Sample Accessibility Component" ref={hostRef} className={styles.wrapper}></div>
     );
 };
 
