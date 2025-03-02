bf556d6638d5fe5c48a4eebd2f3d9e7ec47fc90b
Kobi Krasnoff <krasnoff.kobi@gmail.com>
2025-03-02 21:41:47 +0200

    change library name

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import AccessibilityComponent from './kobikrasnoff/react-accessibility-component/react-accessibility-component'
+import AccessibilityComponent from './component/react-accessibility-component/react-accessibility-component'
 
 function App() {
   const [count, setCount] = useState(0)
